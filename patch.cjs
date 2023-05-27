93615aab8c0905bf61e2ccdaac4605941880d583
Nathan Bolton <github@thinknathan.com>
2023-05-27 12:34:20 -0700

    Fix patch

--- patch.cjs
+++ patch.cjs
@@ -7,38 +7,50 @@ const fs = require('fs');
 const filePath = 'index.d.ts';
 
 const patches = [
-	[`declare type url = {
-	}`, `/**
-	* A URL made from a socket, path, and optionally a fragment.
-	*/
-	declare type url = {
-	 socket: hash;
-	 path: hash;
-	 fragment?: hash;
-	}`],
-	[`/// <reference types="typescript-to-lua/language-extensions" />`, `/// <reference types="@typescript-to-lua/language-extensions" />`]
+	// Add proper definitions for `url`
+	[
+		'declare type url = {\n}',
+		`/**
+			* A URL made from a socket, path, and optionally a fragment.
+			*/
+			declare type url = {
+				socket: hash;
+				path: hash;
+				fragment?: hash;
+			}`,
+	],
+	// Fix path to reference types
+	[
+		`/// <reference types="typescript-to-lua/language-extensions" />`,
+		`/// <reference types="@typescript-to-lua/language-extensions" />`,
+	],
+	// Remove invalid null type
 	[`export let null: any`, ''],
-	[`export function set_texture(path: hash | string, table?: any, buffer: buffer): void`, `export function set_texture(path: hash | string, table: any, buffer: buffer): void`],
+	// Remove invalid optional param in middle of param order
+	[
+		`export function set_texture(path: hash | string, table?: any, buffer: buffer): void`,
+		`export function set_texture(path: hash | string, table: any, buffer: buffer): void`,
+	],
+	// Replace all `any` keywords with unknown `keywords`
 	[/\: any/g, `: unknown`],
 ];
 
 // Load the contents of the file
 fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
+	if (err) {
+		console.error('Error reading file:', err);
+		return;
+	}
 
-  // Make find and replace changes
-	patches.forEach(pair => data = data.replace(pair[0], pair[1]));
+	// Make find and replace changes
+	patches.forEach((pair) => (data = data.replace(pair[0], pair[1])));
 
-  // Save the modified contents back to the file
-  fs.writeFile(filePath, data, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      return;
-    }
-    console.log('File saved successfully.');
-  });
+	// Save the modified contents back to the file
+	fs.writeFile(filePath, data, 'utf8', (err) => {
+		if (err) {
+			console.error('Error writing file:', err);
+			return;
+		}
+		console.log('File saved successfully.');
+	});
 });
-
