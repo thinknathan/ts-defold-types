8d33bf366f4ec054a356f4e56efd642982814a91
thinknathan <github@thinknathan.com>
2024-08-09 19:58:59 -0700

    chore: update comments

--- deprecated.d.ts
+++ deprecated.d.ts
@@ -1,338 +1,231 @@
 declare namespace render {
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STATE_BLEND: number & { readonly __brand: 'render.STATE' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STATE_CULL_FACE: number & { readonly __brand: 'render.STATE' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STATE_DEPTH_TEST: number & { readonly __brand: 'render.STATE' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STATE_POLYGON_OFFSET_FILL: number & {
 		readonly __brand: 'render.STATE';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STATE_STENCIL_TEST: number & {
 		readonly __brand: 'render.STATE';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_DECR: number & { readonly __brand: 'render.STENCIL' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_DECR_WRAP: number & {
 		readonly __brand: 'render.STENCIL';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_INCR: number & { readonly __brand: 'render.STENCIL' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_INCR_WRAP: number & {
 		readonly __brand: 'render.STENCIL';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_INVERT: number & {
 		readonly __brand: 'render.STENCIL';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_KEEP: number & { readonly __brand: 'render.STENCIL' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_REPLACE: number & {
 		readonly __brand: 'render.STENCIL';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const STENCIL_OP_ZERO: number & { readonly __brand: 'render.STENCIL' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const WRAP_CLAMP_TO_BORDER: number & {
 		readonly __brand: 'render.WRAP';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const WRAP_CLAMP_TO_EDGE: number & { readonly __brand: 'render.WRAP' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const WRAP_MIRRORED_REPEAT: number & {
 		readonly __brand: 'render.WRAP';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const WRAP_REPEAT: number & { readonly __brand: 'render.WRAP' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_CONSTANT_ALPHA: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_CONSTANT_COLOR: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_DST_ALPHA: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_DST_COLOR: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_CONSTANT_ALPHA: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_CONSTANT_COLOR: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_DST_ALPHA: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_DST_COLOR: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_SRC_ALPHA: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ONE_MINUS_SRC_COLOR: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_SRC_ALPHA: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_SRC_ALPHA_SATURATE: number & {
 		readonly __brand: 'render.BLEND';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_SRC_COLOR: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BLEND_ZERO: number & { readonly __brand: 'render.BLEND' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_COLOR0_BIT: number & {
 		readonly __brand: 'render.BUFFER';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_COLOR1_BIT: number & {
 		readonly __brand: 'render.BUFFER';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_COLOR2_BIT: number & {
 		readonly __brand: 'render.BUFFER';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_COLOR3_BIT: number & {
 		readonly __brand: 'render.BUFFER';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_COLOR_BIT: number & { readonly __brand: 'render.BUFFER' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_DEPTH_BIT: number & { readonly __brand: 'render.BUFFER' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const BUFFER_STENCIL_BIT: number & {
 		readonly __brand: 'render.BUFFER';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_ALWAYS: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_EQUAL: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_GEQUAL: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_GREATER: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_LEQUAL: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_LESS: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_NEVER: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const COMPARE_FUNC_NOTEQUAL: number & {
 		readonly __brand: 'render.COMPARE_FUNC';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FACE_BACK: number & { readonly __brand: 'render.FACE' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FACE_FRONT: number & { readonly __brand: 'render.FACE' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FACE_FRONT_AND_BACK: number & {
 		readonly __brand: 'render.FACE';
 	};
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FILTER_LINEAR: number & { readonly __brand: 'render.FILTER' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FILTER_NEAREST: number & { readonly __brand: 'render.FILTER' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FORMAT_DEPTH: number & { readonly __brand: 'render.FORMAT' };
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FORMAT_LUMINANCE: number & { readonly __brand: 'render.FORMAT' };
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_R16F:
 		| (number & { readonly __brand: 'render.FORMAT' })
@@ -340,6 +233,7 @@ declare namespace render {
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_R32F:
 		| (number & { readonly __brand: 'render.FORMAT' })
@@ -347,6 +241,7 @@ declare namespace render {
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RG16F:
 		| (number & { readonly __brand: 'render.FORMAT' })
@@ -354,18 +249,18 @@ declare namespace render {
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RG32F:
 		| (number & { readonly __brand: 'render.FORMAT' })
 		| undefined;
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FORMAT_RGB: number & { readonly __brand: 'render.FORMAT' };
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RGB16F:
 		| (number & { readonly __brand: 'render.FORMAT' })
@@ -373,18 +268,18 @@ declare namespace render {
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RGB32F:
 		| (number & { readonly __brand: 'render.FORMAT' })
 		| undefined;
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FORMAT_RGBA: number & { readonly __brand: 'render.FORMAT' };
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RGBA16F:
 		| (number & { readonly __brand: 'render.FORMAT' })
@@ -392,14 +287,13 @@ declare namespace render {
 
 	/**
 	 * May be undefined if the format isn't supported
+	 * @deprecated since v1.9.2
 	 */
 	export const FORMAT_RGBA32F:
 		| (number & { readonly __brand: 'render.FORMAT' })
 		| undefined;
 
-	/**
-	 *
-	 */
+	/** @deprecated since v1.9.2 */
 	export const FORMAT_STENCIL: number & { readonly __brand: 'render.FORMAT' };
 }
 
