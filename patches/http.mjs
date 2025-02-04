939c0e97baa7c0c3453fb99f839807e38c2586fb
thinknathan <github@thinknathan.com>
2025-02-03 17:00:47 -0800

    chore: update http.request signature

--- patches/http.mjs
+++ patches/http.mjs
@@ -5,6 +5,6 @@ export const http = [
 	// function request
 	[
 		'function request(url: string, method: string, callback: any, headers?: any, post_data?: string, options?: any)',
-		'function request(url: string, method: string, callback: ( this: any, id: hash, response: { status: number; response?: string, headers: { [key:string]: string }; path?: string; error?: string; } ) => void, headers?: { [key:string]: string }, post_data?: string, options?: { timeout?: number, path?: string, ignore_cache?: boolean, chunked_transfer?: boolean })',
+		'function request(url: string, method: string, callback: ( this: any, id: hash, response: { status: number; response?: string, headers: { [key:string]: string }; path?: string; url?: string; error?: string; } ) => void, headers?: { [key:string]: string }, post_data?: string, options?: { timeout?: number, path?: string, ignore_cache?: boolean, chunked_transfer?: boolean })',
 	],
 ];
